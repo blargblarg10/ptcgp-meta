c9a4cc1813aba61e5699e261530276e331f513dc
Tyler Somers <tssomersasu@gmail.com>
2025-05-07 19:18:46 -0700

    adds base path for url

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,12 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './context/AuthContext';
 
+// Get the base path from the import.meta.env, defaulting to '/' for development
+const basePath = import.meta.env.BASE_URL || '/';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basePath}>
       <AuthProvider>
         <App />
       </AuthProvider>
